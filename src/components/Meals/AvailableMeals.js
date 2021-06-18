7417cdbf12b69fe1e1ee1e122cf3d7c410d22233
kscheltema <k87scheltema@outlook.com>
2021-06-18 20:20:20 +0200

    loading of data from the firebase

--- src/components/Meals/AvailableMeals.js
+++ src/components/Meals/AvailableMeals.js
@@ -1,42 +1,42 @@
-import { useEffect, useState } from "react";
+// import { useEffect, useState } from "react";
 import Card from "../UI/Card";
 import MealItem from "./MealItem/MealItem";
 import classes from "./AvailableMeals.module.css";
 
-const AvailableMeals = () => {
-  const [meals, setMeals] = useState([]);
-  const [isLoading, setIsLoading] = useState(true);
+// const AvailableMeals = () => {
+//   const [meals, setMeals] = useState([]);
+//   const [isLoading, setIsLoading] = useState(true);
 
-  useEffect(() => {
-    const fetchMeals = async () => {
-      const response = await fetch(
-        "https://burgerbuilder-89b34-default-rtdb.firebaseio.com/meals.json"
-      );
-      const responseData = await response.json();
-      //firebase specific responseData will be an object
+//   useEffect(() => {
+//     const fetchMeals = async () => {
+//       const response = await fetch(
+//         "https://burgerbuilder-89b34-default-rtdb.firebaseio.com/meals.json"
+//       );
+//       const responseData = await response.json();
+//       //firebase specific responseData will be an object
 
-      const loadedMeals = [];
-      for (const key in responseData) {
-        loadedMeals.push({
-          id: key,
-          name: responseData[key].name,
-          description: responseData[key].description,
-          price: responseData[key].price,
-        });
-      }
-      setMeals(loadedMeals);
-      setIsLoading(false);
-    };
-    fetchMeals();
-  }, []); //only need to run this component once thus no dependance required.
+//       const loadedMeals = [];
+//       for (const key in responseData) {
+//         loadedMeals.push({
+//           id: key,
+//           name: responseData[key].name,
+//           description: responseData[key].description,
+//           price: responseData[key].price,
+//         });
+//       }
+//       setMeals(loadedMeals);
+//       setIsLoading(false);
+//     };
+//     fetchMeals();
+//   }, []); //only need to run this component once thus no dependance required.
 
-  if (isLoading) {
-    return (
-      <section className={classes.MealsLoading}>
-        <p>Loading.....</p>
-      </section>
-    );
-  }
+//   if (isLoading) {
+//     return (
+//       <section className={classes.MealsLoading}>
+//         <p>Loading.....</p>
+//       </section>
+//     );
+//   }
 
   const mealsList = meals.map((meal) => (
     <MealItem

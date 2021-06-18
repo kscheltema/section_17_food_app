74108ba0776029d882c65bc7bf973c725e6fa2b2
kscheltema <k87scheltema@outlook.com>
2021-06-18 20:37:29 +0200

    catch and display errors

--- src/components/Meals/AvailableMeals.js
+++ src/components/Meals/AvailableMeals.js
@@ -6,15 +6,19 @@ import classes from "./AvailableMeals.module.css";
 const AvailableMeals = () => {
   const [meals, setMeals] = useState([]);
   const [isLoading, setIsLoading] = useState(true);
+  const [httpError, setHttpError] = useState(null);
 
   useEffect(() => {
     const fetchMeals = async () => {
       const response = await fetch(
         "https://burgerbuilder-89b34-default-rtdb.firebaseio.com/meals.json"
       );
+      if (!response.ok) {
+        throw new Error("Something went wrong!");
+      }
+
       const responseData = await response.json();
       //firebase specific responseData will be an object
-
       const loadedMeals = [];
       for (const key in responseData) {
         loadedMeals.push({
@@ -27,13 +31,24 @@ const AvailableMeals = () => {
       setMeals(loadedMeals);
       setIsLoading(false);
     };
-    fetchMeals();
+    fetchMeals().catch((error) => {
+      setIsLoading(false);
+      setHttpError(error.message);
+    });
   }, []); //only need to run this component once thus no dependance required.
 
   if (isLoading) {
     return (
       <section className={classes.MealsLoading}>
         <p>Loading.....</p>
+      </section>
+    );
+  }
+
+  if (httpError) {
+    return (
+      <section className={classes.MealsError}>
+        <p>{httpError}</p>
       </section>
     );
   }
